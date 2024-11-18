c9c9d6f06d68f0ce5b280ab554a97e7da6341646
Ivan Romadin <ioan.romadin@gmail.com>
2024-11-18 14:48:52 +0300

    fix: update dto's: remove serial id's

--- internal/dto/dtos.go
+++ internal/dto/dtos.go
@@ -3,13 +3,11 @@ package dto
 import "time"
 
 type User struct {
-	Id             int `db:"id,int"`
 	TelegramId     int `db:"telegram_id,int"`
 	TelegramChatId int `db:"telegram_chat_id,int"`
 }
 
 type Vacancy struct {
-	Id           int       `db:"id,int"`
 	VacancyId    int       `db:"vacancy_id,int"`
 	VacancyName  int       `db:"vacancy_name"`
 	RoleId       int       `db:"role_id,int"`
@@ -19,7 +17,6 @@ type Vacancy struct {
 }
 
 type Role struct {
-	Id       int    `db:"id,int"`
 	RoleId   int    `db:"role_id,int"`
 	RoleName string `db:"role_name"`
 }
--- internal/infrastructure/repository/vacancies_db.go
+++ internal/infrastructure/repository/vacancies_db.go
@@ -6,4 +6,16 @@ import (
 	"github.com/wanomir/hh-reporter/internal/dto"
 )
 
+// TODO: finish writing basic CRUD's
+// https://arc.net/l/quote/vylfjpww
+// INSERT INTO vacancies (vacancy_id, vacancy_name, role_id,  experience, url, published_at)
+// VALUES
+//     (
+//       1, 'vacancy', 1, 'from0To3', 'https://some-website', NOW()
+//     )
+// ON CONFLICT ()
+// DO
+// UPDATE
+// SET content=%(content)s, last_analyzed = NOW();
+
 func (db *PostgresDB) InsertVacancy(ctx context.Context, vacancy dto.Vacancy) {}
